db2f77f1c3fc0f82d59112f966e60b4f1e39b353
Jess Daniel <jessdaniel474@gmail.com>
2019-10-04 13:31:24 -0400

    Add all article cards to DOM

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,57 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+// Get Data from API
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+.then(response => {
+    console.log(response);
+    const bootstrap = response.data.articles.bootstrap;
+    const javascript = response.data.articles.javascript;
+    const jquery = response.data.articles.jquery;
+    const node = response.data.articles.node;
+    const technology = response.data.articles.technology;
+    
+    const catagory = [bootstrap, javascript, jquery, node, technology];
+    catagory.forEach(e => {
+        e.forEach(article => {
+            console.log(article);
+            const newArticle = createCard(article)
+            cardContainer.appendChild(newArticle);
+        })
+    })
+});
+
+// Get card container
+const cardContainer = document.querySelector(".cards-container");
+
+function createCard(obj) {
+    // create elements
+    const card = document.createElement("div");
+    const headline = document.createElement("div");
+    const author = document.createElement("div");
+    const imgContainer = document.createElement("div");
+    const img = document.createElement("img");
+    const byLine = document.createElement("span");
+
+    // create structure
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(img);
+    author.appendChild(byLine);
+
+    // add classes
+    card.classList.add("card");
+    headline.classList.add("headline");
+    author.classList.add("author");
+    imgContainer.classList.add("img-container");
+
+    // add content
+    headline.textContent = obj.headline
+    img.src = obj.authorPhoto;
+    byLine.textContent = `By ${obj.authorName}`;
+
+    return card;
+}
+
